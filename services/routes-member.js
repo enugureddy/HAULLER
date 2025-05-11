ea7a3b8ebd1d07bb119788200e0d782f9d16a75b
Thirus224849242 <s224849242@deakin.edu.au>
2025-05-11 14:01:43 +1000

    implemented session

--- services/routes-member.js
+++ services/routes-member.js
@@ -1,36 +1,37 @@
 const controller = require("./controller-member")
-
+const { isAdminLoggedIn } = require('../middleware/auth');
 module.exports=function(member){
+   
     member.route("/").get(controller.login)
     member.route("/loginverify").post(controller.loginverify)
     member.route("/member-register").get(controller.register)
     member.route("/registerpost").post(controller.registerpost)
     member.route('/member-forgotpassword').get(controller.forgotpassword)
     member.route('/sendpassword').post(controller.sendpassword)
-    member.route("/viewadds").get(controller.viewadds) 
+    member.route("/viewadds").get(isAdminLoggedIn,controller.viewadds) 
    // member.route('/uploadview').get(controller.uploadview)
    // member.route('/uploadaction').post(controller.uploadaction)
-   member.route("/uploadview").get(controller.uploadView)
+   member.route("/uploadview").get(isAdminLoggedIn,controller.uploadView)
 
    //do the action from the form
-   member.route("/uploadaction").post(controller.uploadAction)
+   member.route("/uploadaction").post(isAdminLoggedIn,controller.uploadAction)
 
-   member.route("/delete/:id").get(controller.delete)
+   member.route("/delete/:id").get(isAdminLoggedIn,controller.delete)
 
 
-   member.route("/updatedet/:id").get(controller.updatedet)
+   member.route("/updatedet/:id").get(isAdminLoggedIn,controller.updatedet)
 
-   member.route("/updateimg/:id").get(controller.updateimg)
+   member.route("/updateimg/:id").get(isAdminLoggedIn,controller.updateimg)
 
-   member.route("/updateimgpost").post(controller.updateimgpost)
+   member.route("/updateimgpost").post(isAdminLoggedIn,controller.updateimgpost)
 
-   member.route("/updatedetpost").post(controller.updatedetpost)
+   member.route("/updatedetpost").post(isAdminLoggedIn,controller.updatedetpost)
    
-   member.route("/dadd").get(controller.dadd)
-   member.route("/dacc").get(controller.dacc)
-   member.route("/uacc").get(controller.uacc)
-   member.route("/uacc").post(controller.uaccpost)
-   member.route("/view/:id").get(controller.view)
+   member.route("/dadd").get(isAdminLoggedIn,controller.dadd)
+   member.route("/dacc").get(isAdminLoggedIn,controller.dacc)
+   member.route("/uacc").get(isAdminLoggedIn,controller.uacc)
+   member.route("/uacc").post(isAdminLoggedIn,controller.uaccpost)
+   member.route("/view/:id").get(isAdminLoggedIn,controller.view)
   
     member.route('/logout').get(controller.logout)
 
